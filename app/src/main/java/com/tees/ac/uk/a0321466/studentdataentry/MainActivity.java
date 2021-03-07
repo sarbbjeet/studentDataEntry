48354bba89876123e7f583c4753d3d314c40ebc4
sarbjeet <sarbjeets18@gmail.com>
2021-03-07 12:51:27 +0000

    add button is now active in the database

--- app/src/main/java/com/tees/ac/uk/a0321466/studentdataentry/MainActivity.java
+++ app/src/main/java/com/tees/ac/uk/a0321466/studentdataentry/MainActivity.java
@@ -3,12 +3,61 @@ package com.tees.ac.uk.a0321466.studentdataentry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public databaseHelper databasehandler;
+
+    //defined view components
+
+    EditText et_name, et_age, et_studentId;
+    studentModule studentmodel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        databasehandler =new databaseHelper(MainActivity.this);
+        //view components reference
+        et_name= findViewById(R.id.et_name);
+        et_age= findViewById(R.id.et_age);
+        et_studentId= findViewById(R.id.et_id);
+
+        //add button (add student contacts to the database)
+    ((Button)findViewById(R.id.btnAdd)).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+           //check validation (edit text should not empty)
+            try{
+                studentmodel =new studentModule(-1,(et_name.getText().toString()),(Integer.parseInt(et_age.getText().toString())),
+                        (long) Integer.parseInt(et_studentId.getText().toString()));
+                //return true if data successfully inserted to the database
+                if (databasehandler.insertContact(studentmodel)) {
+                    Toast.makeText(MainActivity.this, "successfully insert data to database", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Error to insert data to database", Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch(Exception err){
+                Toast.makeText(MainActivity.this,err.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    });
+
+        //View All student contacts data from the database
+        ((Button)findViewById(R.id.btnViewAll)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/tees/ac/uk/a0321466/studentdataentry/databaseHelper.java
+++ app/src/main/java/com/tees/ac/uk/a0321466/studentdataentry/databaseHelper.java
@@ -1,12 +1,22 @@
 package com.tees.ac.uk.a0321466.studentdataentry;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
 public class databaseHelper extends SQLiteOpenHelper {
+
+    //database parameters
+    public static final String DATABASE_NAME = "students.db";
+    public static final String CONTACTS_TABLE_NAME = "contacts";
+    public static final String CONTACTS_COLUMN_ID = "id";
+    public static final String CONTACTS_COLUMN_NAME = "name";
+    public static final String CONTACTS_COLUMN_AGE = "age";
+    public static final String CONTACTS_COLUMN_STUDENT_ID = "studentID";
 
 
     public databaseHelper(@Nullable Context context) {
@@ -16,12 +26,35 @@ public class databaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-   
+        //we can create query in the form of string as written below()
+//        String createTableStatement= "create table contacts " +
+//                "(id integer primary key, name text,age int,studentID int)";
+        //write query using global variables
+        String createTableStatement= "create table " + CONTACTS_TABLE_NAME +
+                " ( " + CONTACTS_COLUMN_ID + " integer primary key, " + CONTACTS_COLUMN_NAME + " text, "
+                   + CONTACTS_COLUMN_AGE + " int, " + CONTACTS_COLUMN_STUDENT_ID + " int)";
+        db.execSQL(createTableStatement);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
 
+    public boolean insertContact(studentModule studentmodel){
+    SQLiteDatabase db = this.getWritableDatabase();
+    ContentValues cv= new ContentValues();
+    cv.put(CONTACTS_COLUMN_NAME, studentmodel.getName().toString());
+    cv.put(CONTACTS_COLUMN_AGE, studentmodel.getAge());
+    cv.put(CONTACTS_COLUMN_STUDENT_ID, studentmodel.getStudentId());
+    long insert= db.insert(CONTACTS_TABLE_NAME,null,cv);
+    if(insert==-1){
+        return false;
+    }
+    else{
+        return true;
+    }
     }
 }
